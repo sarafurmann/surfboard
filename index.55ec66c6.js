082baaeae52a5792422c04b6a3c46cef2821fc33
Сара Фурманова <sarafurmann@192.168.1.5>
2022-04-04 18:56:26 +0300

    Updates

---
+++ index.55ec66c6.js
@@ -0,0 +1,2 @@
+const slider=$(".products__list").bxSlider({pager:!1,controls:!1,wrapperClass:null});$(".products__arrow--left").click((r=>{r.preventDefault(),slider.goToPrevSlide()})),$(".products__arrow--right").click((r=>{r.preventDefault(),slider.goToNextSlide()}));
+//# sourceMappingURL=index.55ec66c6.js.map
--- index.73d6af49.js
+++
@@ -1,2 +0,0 @@
-const slider=$(".products__list").bxSlider({pager:!1,controls:!1});$(".products__arrow--left").click((r=>{r.preventDefault(),slider.goToPrevSlide()})),$(".products__arrow--right").click((r=>{r.preventDefault(),slider.goToNextSlide()}));
-//# sourceMappingURL=index.73d6af49.js.map
